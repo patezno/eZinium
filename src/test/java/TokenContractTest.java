5fd8d60439daee4cf927c477036510833c366ae9
unknown <patezno97@gmail.com>
2019-02-20 17:19:01 +0100

    started project and added classes

---
+++ src/test/java/TokenContractTest.java
@@ -0,0 +1,24 @@
+import org.junit.Test;
+
+public class TokenContractTest {
+
+    @Test
+    public void payable_test() {
+
+        Address rick = new Address();
+        rick.generateKeyPair();
+        TokenContract ricknillos = new TokenContract(rick);
+        ricknillos.addOwner(rick.getPK(), 100d);
+        Address morty = new Address();
+        morty.generateKeyPair();
+
+        morty.addEZI(20d);
+
+        // verifico la transferencia de entradas
+        ricknillos.payable(morty.getPK(), morty.getBalance());
+        assertEquals(4d, ricknillos.balanceOf(morty.getPK()), 0d);   
+
+        // verifico la trasnferencia de EZI
+        assertEquals(20d, ricknillos.owner().getBalance(), 0d);
+    }
+}
